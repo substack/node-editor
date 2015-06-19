54553ff868cf62d2aeeb3cae08ee29e18fe7e9c4
clement <maboiteaspam@wanadoo.fr>
2015-06-19 19:43:10 +0800

    make use of an array of editor instead of a single string test each editor in series, with help async, skip to next on error event, skip to next if an editor has trigger exit event if no editor would be found at all, an ENOENT error is sent to the callback, rely on errno-codes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var spawn = require('child_process').spawn;
+var _ = require('underscore');
+var async = require('async');
+var Error = require ("errno-codes");
 
 module.exports = function (file, opts, cb) {
     if (typeof opts === 'function') {
@@ -6,15 +9,48 @@ module.exports = function (file, opts, cb) {
         opts = {};
     }
     if (!opts) opts = {};
-    
-    var ed = /^win/.test(process.platform) ? 'notepad' : 'vim';
-    var editor = opts.editor || process.env.VISUAL || process.env.EDITOR || ed;
-    var args = editor.split(/\s+/);
-    var bin = args.shift();
-    
-    var ps = spawn(bin, args.concat([ file ]), { stdio: 'inherit' });
-    
-    ps.on('exit', function (code, sig) {
-        if (typeof cb === 'function') cb(code, sig)
+
+    var asyncTasks = [];
+    var edInvoked = false;
+    var wellKnownEditors = opts.editors || [];
+
+    var editor = opts.editor || process.env.VISUAL || process.env.EDITOR;
+
+    if( editor ){
+      wellKnownEditors.push(editor)
+    }
+
+    if(/^win/.test(process.platform)){
+      wellKnownEditors = wellKnownEditors.concat(['start notepad++', 'notepad'])
+    } else {
+      wellKnownEditors = wellKnownEditors.concat(['vim', 'vi', 'pico', 'nano'])
+    }
+
+    _.uniq(wellKnownEditors).forEach(function(ed){
+      asyncTasks.push(function invokeEditor(then){
+        if(edInvoked) return then();
+
+        var args = ed.split(/\s+/);
+        var bin = args.shift();
+
+        var ps = spawn(bin, args.concat([ file ]), { stdio: 'inherit' });
+
+        ps.on('error', function () {
+          ps.removeAllListeners('exit');
+          then();
+        });
+
+        ps.on('exit', function (code, sig) {
+          edInvoked = true;
+          if (typeof cb === 'function') cb(code, sig)
+          then();
+        });
+
+      });
+    });
+    async.series(asyncTasks, function allTested(){
+      if(!edInvoked){
+        if (typeof cb === 'function') cb(Error.get (Error.ENOENT))
+      }
     });
 };
