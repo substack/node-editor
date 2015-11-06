0f19089991c4ba102abb0c916ad010d2f880c2db
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-11-07 00:09:52 +0100

    Support some editors are using special characters in their name

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ module.exports = function (file, opts, cb) {
         opts = {};
     }
     if (!opts) opts = {};
-    
+
     var ed = /^win/.test(process.platform) ? 'notepad' : 'vim';
     var editor = opts.editor || process.env.VISUAL || process.env.EDITOR || ed;
     var args = editor.split(/\s+/);
-    var bin = args.shift();
-    
+    var bin = args.join("\\");
+
     var ps = spawn(bin, args.concat([ file ]), { stdio: 'inherit' });
-    
+
     ps.on('exit', function (code, sig) {
         if (typeof cb === 'function') cb(code, sig)
     });
